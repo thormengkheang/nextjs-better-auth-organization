b0887a5618e57a1af52ad76ae687e338a1db86b2
Mengkheang Thor <thormengkheang@gmail.com>
2025-06-30 22:29:55 +0700

    replace dashboard

--- src/app/(root)/(dashboard)/dashboard/page.tsx
+++ src/app/(root)/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,44 @@
-export default function Page() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import UserCard from "./user-card";
+import { OrganizationCard } from "./organization-card";
+import AccountSwitcher from "@/components/account-switch";
+
+export default async function DashboardPage() {
+  const [session, activeSessions, deviceSessions, organization] =
+    await Promise.all([
+      auth.api.getSession({
+        headers: await headers(),
+      }),
+      auth.api.listSessions({
+        headers: await headers(),
+      }),
+      auth.api.listDeviceSessions({
+        headers: await headers(),
+      }),
+      auth.api.getFullOrganization({
+        headers: await headers(),
+      }),
+    ]).catch((e) => {
+      console.log(e);
+      throw redirect("/sign-in");
+    });
   return (
-    <>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
+    <div className="w-full">
+      <div className="flex gap-4 flex-col">
+        <AccountSwitcher
+          sessions={JSON.parse(JSON.stringify(deviceSessions))}
+        />
+        <UserCard
+          session={JSON.parse(JSON.stringify(session))}
+          activeSessions={JSON.parse(JSON.stringify(activeSessions))}
+        />
+        <OrganizationCard
+          session={JSON.parse(JSON.stringify(session))}
+          activeOrganization={JSON.parse(JSON.stringify(organization))}
+        />
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-    </>
+    </div>
   );
 }
