681f00e3684364d13585084ed44a75191770ab20
Mengkheang Thor <thormengkheang@gmail.com>
2025-07-01 15:08:59 +0700

    add org switcher

--- src/components/org-switcher.tsx
+++ src/components/org-switcher.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import * as React from "react";
-import {
-  AudioWaveform,
-  ChevronsUpDown,
-  Command,
-  GalleryVerticalEnd,
-  Plus,
-} from "lucide-react";
-
+import React from "react";
+import { ChevronsUpDown } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
@@ -24,42 +16,31 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { client, useListOrganizations } from "@/lib/auth-client";
+import { Session, ActiveOrganization } from "@/lib/auth-types";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-// interface Team {
-//   name: string;
-//   logo: React.ComponentType<{ className?: string }>;
-//   plan: string;
-// }
+interface OrgSwitcherProps {
+  session: Session | null;
+  activeOrg: ActiveOrganization | null;
+}
 
-// interface TeamSwitcherProps {
-//   teams: Team[];
-// }
-
-const teams = [
-  {
-    name: "Acme Inc",
-    logo: GalleryVerticalEnd,
-    plan: "Enterprise",
-  },
-  {
-    name: "Acme Corp.",
-    logo: AudioWaveform,
-    plan: "Startup",
-  },
-  {
-    name: "Evil Corp.",
-    logo: Command,
-    plan: "Free",
-  },
-];
-
-export function OrgSwitcher() {
+export function OrgSwitcher(props: OrgSwitcherProps) {
+  const { activeOrg } = props;
+  const { data: orgs } = useListOrganizations();
   const { isMobile } = useSidebar();
-  const [activeTeam, setActiveTeam] = React.useState(teams[0]);
 
-  if (!activeTeam) {
-    return null;
-  }
+  const [optimisticOrg, setOptimisticOrg] =
+    React.useState<ActiveOrganization | null>(activeOrg);
+
+  const handleOnOrgItemClick = async (orgId: string) => {
+    await client.organization.setActive({
+      organizationId: orgId,
+    });
+    setOptimisticOrg(
+      orgs?.find((org) => org.id === orgId) as ActiveOrganization | null,
+    );
+  };
 
   return (
     <SidebarMenu>
@@ -70,14 +51,19 @@ export function OrgSwitcher() {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <activeTeam.logo className="size-4" />
-              </div>
+              <Avatar className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <AvatarImage
+                  className="object-cover size-4 rounded-none"
+                  src={optimisticOrg?.logo || undefined}
+                />
+                <AvatarFallback className="rounded-none">
+                  {optimisticOrg?.name?.charAt(0) || "P"}
+                </AvatarFallback>
+              </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">
-                  {activeTeam.name}
+                  {optimisticOrg?.name || "Personal"}
                 </span>
-                <span className="truncate text-xs">{activeTeam.plan}</span>
               </div>
               <ChevronsUpDown className="ml-auto" />
             </SidebarMenuButton>
@@ -89,28 +75,27 @@ export function OrgSwitcher() {
             sideOffset={4}
           >
             <DropdownMenuLabel className="text-xs text-muted-foreground">
-              Teams
+              Organizations
             </DropdownMenuLabel>
-            {teams.map((team, index) => (
+            {orgs?.map((org, index) => (
               <DropdownMenuItem
-                key={team.name}
-                onClick={() => setActiveTeam(team)}
+                key={org.slug}
+                onClick={() => handleOnOrgItemClick(org.id)}
                 className="gap-2 p-2"
               >
-                <div className="flex size-6 items-center justify-center rounded-sm border">
-                  <team.logo className="size-4 shrink-0" />
-                </div>
-                {team.name}
+                <Avatar className="flex size-6 items-center justify-center rounded-sm border">
+                  <AvatarImage
+                    className="object-cover size-4 shrink-0"
+                    src={org?.logo || undefined}
+                  />
+                  <AvatarFallback className="size-4 shrink-0">
+                    {org?.name?.charAt(0) || "P"}
+                  </AvatarFallback>
+                </Avatar>
+                {org.name}
                 <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
               </DropdownMenuItem>
             ))}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="gap-2 p-2">
-              <div className="flex size-6 items-center justify-center rounded-md border bg-background">
-                <Plus className="size-4" />
-              </div>
-              <div className="font-medium text-muted-foreground">Add team</div>
-            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </SidebarMenuItem>
